56ffda58ef6ff4d318e85553f6dc036a15099bd9
RanDY <baby6675525@gmail.com>
2019-05-19 15:00:51 +0800

    replay

---
+++ app/src/main/java/com/tom/guess/MaterialActivity.kt
@@ -0,0 +1,123 @@
+package com.tom.guess
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity;
+
+import kotlinx.android.synthetic.main.activity_material.*
+import kotlinx.android.synthetic.main.content_material.*
+
+class MaterialActivity : AppCompatActivity() {
+    private val REQUEST_RECORD: Int=100
+    val secretNumber=SecretNumber()
+    val TAG=MaterialActivity::class.java.simpleName
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG,"onCreate:")
+        setContentView(R.layout.activity_material)
+        setSupportActionBar(toolbar)
+        Log.d(TAG,"secretNumber:"+secretNumber.secret)
+
+        fab.setOnClickListener { view ->
+            replay()
+        }
+        counter.setText(secretNumber.count.toString())
+        //Log.d(TAG,"secretNumber:"+secretNumber.secret)
+        val count=getSharedPreferences("guess", Context.MODE_PRIVATE)
+            .getInt("REC_COUNTER",-1)
+        val nick=getSharedPreferences("guess", Context.MODE_PRIVATE)
+            .getString("REC_NICKNAME",null)
+        Log.d(TAG,"data:"+count+"/"+nick)
+    }
+
+    private fun replay() {
+        AlertDialog.Builder(this)
+            .setTitle("Replay game")
+            .setMessage("Are U Sure?")
+            .setPositiveButton(getString(R.string.ok), { dialog, which ->
+                secretNumber.reset()
+                counter.setText(secretNumber.count.toString())
+                number.setText("")
+                Log.d(TAG, "secretNumber:" + secretNumber.secret)
+            })
+            .setNeutralButton("Cancel", null)
+            .show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG,"onStart:")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG,"onStop:")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG,"onPause:")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG,"onRestart:")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG,"onResume:")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG,"onDestroy:")
+    }
+
+    fun check(view: View){
+        val n=number.text.toString().toInt()
+        Log.d(TAG,"number:"+n)
+        val diff=secretNumber.validate(n)
+        var count=secretNumber.count
+        var message=getString(R.string.bingo)
+        if(diff<0){
+            message=getString(R.string.bigger)
+        }else if(diff>0){
+            message=getString(R.string.smaller)
+        }
+        if(count<3&&diff==0){
+            message="Excellent! The number is $n"
+        }
+        counter.setText(secretNumber.count.toString())
+        //Toast.makeText(this,message,Toast.LENGTH_LONG).show()//浮動訊息
+        AlertDialog.Builder(this)
+            .setTitle(getString(R.string.message))
+            .setMessage(message)
+            .setPositiveButton(getString(R.string.ok),{dialog,which->
+                if(diff==0){
+                    val intent=Intent(this,RecordActivity::class.java)
+                    intent.putExtra("COUNTER",secretNumber.count)
+                    //startActivity(intent)
+                    startActivityForResult(intent,REQUEST_RECORD)
+                }
+            })
+            .show()
+        number.setText("")
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode==REQUEST_RECORD){
+            if(resultCode== Activity.RESULT_OK){
+                val nickname=data?.getStringExtra("Nick")
+                Log.d(TAG,"onActivityResult:"+nickname)
+                replay()
+            }
+        }
+    }
+}
